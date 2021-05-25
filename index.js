0d1af3d3425439a036770fbeceb892b81f98b88f
ksetoue <karoline.kimiko@gmail.com>
2021-05-25 01:03:37 -0300

    :sparkles: audio recognition test

---
+++ index.js
@@ -0,0 +1,33 @@
+const speech = require('@google-cloud/speech');
+const fs = require('fs');
+
+async function main() {
+    const client = new speech.SpeechClient();
+    const filename = './audio.mp3';
+
+    const file = fs.readFileSync(filename);
+    const audioBytes = file.toString('base64');
+
+    const audio = {
+        content: audioBytes
+    };
+
+    const config = {
+        encoding: 'MP3',
+        sampleRateHertz: 16000,
+        languageCode: 'en-US'
+    }
+
+    const request = {
+        audio: audio,
+        config: config
+    }
+
+    const [response] = await client.recognize(request);
+
+    const transcription = response.results.map(result => 
+        result.alternatives[0].transcript).join('\n');
+        console.log(`Transcription: ${transcription}`);
+}
+
+main().catch(console.error);
